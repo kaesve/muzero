Recursion Error hopefully fixed. Scaling of hidden state.

Main.py
@@ -73,22 +73,25 @@ def learnM0():
     b = g.getInitialState()
     g.display(b)
     # TODO: Important! Perturb sequences before the 0th observation with slight noise to prevent state collapse to 0.
-    history = deque([np.random.randn(*b.shape) * 1e-8] * (net_args.observation_length - 1) + [b], maxlen=net_args.observation_length)
+    history = deque([np.random.randn(*b.shape) * 0] * (net_args.observation_length - 1) + [b], maxlen=net_args.observation_length)
     mcts = MuZeroMCTS(g, hex_net, args)
 
     player = 1
-    for i in range(10):
+    while g.getGameEnded(b, player) == 0:
         canon = g.getCanonicalForm(b, player)
         if player == 1:
             obs = np.array(history)
-            pi_visit = mcts.runMCTS(obs, temp=1)
+            pi_visit, _ = mcts.runMCTS(obs, temp=1)
             a = np.random.choice(len(pi_visit), p=pi_visit)
         else:
-            a = np.argmax(g.getLegalMoves(canon, 1))
+            moves = g.getLegalMoves(canon, 1)
+            a = np.random.choice(len(moves), p=moves/np.sum(moves))
 
-        b, player = g.getNextState(b, player, a)
+        b, _, player = g.getNextState(b, a, player)
         history.append(b)
         g.display(b)
+
+    print("MuZero won" if player == -1 else "Random player won")
 
 
 if __name__ == "__main__":

MuZero/MuMCTS.py
@@ -67,7 +67,7 @@ class MuZeroMCTS:
 
         ucb = self.Ps[s][a] * np.sqrt(self.Ns[s]) / (1 + visit_count) * exploration_factor  # Exploration
         ucb += self.minmax.normalize(q_value)                                               # Exploitation
-        return ucb
+        return ucb + np.random.normal(loc=0, scale=1e-4)  # Add slight Gaussian noise to break symmetry.
 
     def runMCTS(self, observations, temp=1):
         """
@@ -81,7 +81,7 @@ class MuZeroMCTS:
             v: (float) Estimated value of the root state.
         """
         latent_state = self.neural_net.encode(observations)
-        s = latent_state.numpy().tostring()  # Hashable representation
+        s = latent_state.tostring()  # Hashable representation
 
         # Refresh value bounds in the tree
         self.minmax.refresh()
@@ -129,18 +129,17 @@ class MuZeroMCTS:
         Returns:
             v: the negative of the value of the current canonicalBoard
         """
-        s = latent_state.numpy().tostring()  # Hashable representation.
+        s = latent_state.tostring()  # Hashable representation.
 
         ### ROLLOUT
         if s not in self.Ps:
             # leaf node
             self.Ps[s], v = self.neural_net.predict(latent_state)
+            self.Ns[s] = 1
 
             if root:  # Add Dirichlet noise to the root prior and mask illegal moves.
                 self.modify_root_prior(s)
 
-            self.Ps[s] /= np.sum(self.Ps[s])
-            self.Ns[s] = 0
             return self.turn_indicator(count) * v
 
         ### SELECTION
@@ -152,6 +151,11 @@ class MuZeroMCTS:
         if s in self.Vs:
             confidence_bounds = np.array(confidence_bounds) * self.Vs[s][:-1]  # Omit resignation.
         a = np.argmax(confidence_bounds)
+
+        if count > 25:
+            print(count)
+            print(confidence_bounds)
+            print(latent_state)
 
         ### EXPANSION
         # Perform a forward pass using the dynamics function (unless already known in the transition table)

hex/MuZeroModel/NNet.py
@@ -21,11 +21,8 @@ def scalar_loss(prediction, target):
     return (prediction - target) ** 2
 
 
-def scale_latent_state(latent_state):
-    s_min = tf.reduce_min(latent_state)
-    s_max = tf.reduce_max(latent_state)
-    s_scale = s_max - s_min
-    return (latent_state - s_min) / s_scale
+def scale_latent_state(s):
+    return (s - s.min()) / (1 + s.max() - s.min()) + 1 / np.prod(s.shape)
 
 
 class NNetWrapper(MuZeroNeuralNet):
@@ -79,17 +76,17 @@ class NNetWrapper(MuZeroNeuralNet):
 
     def encode(self, observations):
         observations = observations[np.newaxis, ...]
-        latent_state = self.neural_net.encoder(observations)[0]
+        latent_state = self.neural_net.encoder.predict(observations)[0]
         return scale_latent_state(latent_state)
 
     def forward(self, latent_state, action):
         a_plane = np.zeros((self.board_x, self.board_y))
         a_plane[action // self.board_x][action % self.board_y] = 1
 
-        latent_state = tf.reshape(latent_state, (-1, self.board_x, self.board_y))
-        a_plane = tf.reshape(a_plane, (-1, self.board_x, self.board_y))
+        latent_state = latent_state.reshape((-1, self.board_x, self.board_y))
+        a_plane = a_plane.reshape((-1, self.board_x, self.board_y))
 
-        r, s_next = self.neural_net.dynamics([latent_state, a_plane])
+        r, s_next = self.neural_net.dynamics.predict([latent_state, a_plane])
 
         r_real = support_to_scalar(r[0], self.net_args.support_size)
 
@@ -99,8 +96,8 @@ class NNetWrapper(MuZeroNeuralNet):
         """
         board: np array with board
         """
-        latent_state = tf.reshape(latent_state, (-1, self.board_x, self.board_y))
-        pi, v = self.neural_net.predictor(latent_state)
+        latent_state = latent_state.reshape((-1, self.board_x, self.board_y))
+        pi, v = self.neural_net.predictor.predict(latent_state)
 
         v_real = support_to_scalar(v[0], self.net_args.support_size)
 